27115708f28d340f01d1004b22a60bc20a18b616
melakeltsegaye <melakeltsegaye@gmail.com>
2026-06-01 09:29:58 +0300

    db.js validation added

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,9 @@ import { ENV } from "./env.js"
 
 export const connectDB = async () => {
     try {
+        if(!ENV.DB_URL) {
+            throw new error("DB_URL is not defiend in enviroment variabel")
+        }
         const conn = await mongoose.connect(ENV.DB_URL)
         console.log("✅ db conencted successfuly", conn.connection.host)
     } catch (error) {
