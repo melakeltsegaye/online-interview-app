4857e8d3dca2c57bdfc3abd6102a6c879ebad683
melakeltsegaye <melakeltsegaye@gmail.com>
2026-06-02 21:11:13 +0300

    hope it works "clerk"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
-import { Show, SignInButton, SignUpButton, UserButton, SignedOut, SignedIn} from '@clerk/react'
+import { SignInButton, SignUpButton, UserButton, SignedOut, SignedIn} from '@clerk/clerk-react'
 import './App.css'
+
+
 
 function App() {
 
 
   return (
-    <>
+    <div>
+      
     <h1>wellcome</h1>
     <header>
-           <header>
         <SignedOut>
            <SignInButton mode="modal">
       <button className="w-36 p-4 bg-gray-900 text-white rounded">
@@ -20,9 +22,8 @@ function App() {
         <SignedIn>
           <UserButton />
         </SignedIn>
-      </header>
     </header>
-          </>
+          </div>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ClerkProvider } from '@clerk/react'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
+if (!PUBLISHABLE_KEY){
+  throw new Error("missing Publishable key")
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <ClerkProvider>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <App />
   </ClerkProvider>
   </StrictMode>
